aa641c96fa4c8d4eb8635ebd4188430cbbd01c25
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-12-30 17:10:28 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //always end code line with ;
+            //Write line allows us to write something on the screen
+            Console.WriteLine("Hello World");
+            //Helps me to view the Outcome of my console.
+    
+            Console.ReadLine();
+        }
+    }
+}
